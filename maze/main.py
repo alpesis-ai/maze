f242f007930efab8e61625bcb3d5600a5542e042
K <kwailamchan@hotmail.com>
2018-03-17 01:21:21 +0800

    [master] added queue, priority_queue on breath_first_search. 	modified:   main.py 	modified:   maze.py

--- maze/main.py
+++ maze/main.py
@@ -3,8 +3,13 @@ from maze import Maze
 
 if __name__ == '__main__':
 
-    maze_t = Maze() 
-    path = maze_t.breadth_first()
-    print("breath_first_search:\n", maze_t.visualize_path(path))
-    path = maze_t.depth_first()
-    print("depth_first_search:\n", maze_t.visualize_path(path))
+    maze_t = Maze()
+ 
+    path, path_cost = maze_t.breath_first_deque()
+    print("breath_first_deque: ", path_cost, "\n", maze_t.visualize_path(path))
+
+    path, path_cost = maze_t.breath_first_queue()
+    print("breath_first_queue: ", path_cost, "\n", maze_t.visualize_path(path))
+
+    path, path_cost = maze_t.breath_first_priority_queue()
+    print("breath_first_priority_queue: ", path_cost, "\n", maze_t.visualize_path(path))
--- maze/maze.py
+++ maze/maze.py
@@ -1,20 +1,31 @@
 import numpy as np
 from enum import Enum
 from collections import deque
+from queue import Queue, PriorityQueue
 
 
 class Action(Enum):
-
-    LEFT = (0, -1)
-    RIGHT = (0, 1)
-    UP = (-1, 0)
-    DOWN = (1, 0)
+    """
+    Action: [delta_row, delta_col, cost]
+    """
+    LEFT = (0, -1, 1)
+    RIGHT = (0, 1, 1)
+    UP = (-1, 0, 1)
+    DOWN = (1, 0, 1)
 
     def __str__(self):
         if self == self.LEFT:  return '<'
         elif self == self.RIGHT: return '>'
         elif self == self.UP: return '^'
         elif self == self.DOWN: return 'v'
+
+    @property
+    def cost(self):
+        return self.value[2]
+
+    @property
+    def delta(self):
+        return (self.value[0], self.value[1])
 
 
 class Maze(object):
@@ -72,15 +83,17 @@ class Maze(object):
         return solution
 
 
-    def breadth_first(self):
+    def breath_first_deque(self):
         path = []
+        path_cost = 0
+
         queue = deque()
+        queue.append(self.start)
         visited = []
 
         branch = {}
         found = False
-
-        queue.append(self.start)
+        
         while len(queue) > 0:
             current_node = queue.popleft()
             if current_node == self.goal:
@@ -89,8 +102,8 @@ class Maze(object):
                 break;
             else:
                 for x in self.valid_actions(current_node):
-                    delta = x.value
-                    next_node = (current_node[0] + delta[0], current_node[1] + delta[1])
+                    next_node = (current_node[0] + x.delta[0], current_node[1] + x.delta[1])
+                    path_cost += x.cost
                     if next_node not in visited:
                         queue.append(next_node)
                         visited.append(current_node)
@@ -102,9 +115,76 @@ class Maze(object):
                 n = branch[n][0]
             path.append(branch[n][1])
 
-        return path[::-1]
+        return path[::-1], path_cost
 
 
-    def depth_first(self):
+    def breath_first_queue(self):
         path = []
-        return path[::-1] 
+        path_cost = 0
+
+        queue = Queue()
+        queue.put(self.start)
+        visited = set(self.start)
+
+        branch = {}
+        found = False
+        while not queue.empty():
+            current_node = queue.get()
+            if current_node == self.goal:
+                print('Found a path.')
+                found = True
+                break
+            else:
+                for action in self.valid_actions(current_node):
+                    next_node = (current_node[0] + action.delta[0], current_node[1] + action.delta[1])
+                    path_cost += action.cost
+                    if next_node not in visited:
+                        queue.put(next_node)
+                        visited.add(next_node)
+                        branch[next_node] = (path_cost, current_node, action)
+
+        if found:
+            n = self.goal
+            path_cost = branch[n][0]
+            while branch[n][1] != self.start:
+                path.append(branch[n][2])
+                n = branch[n][1]
+            path.append(branch[n][2])
+
+        return path[::-1], path_cost
+       
+ 
+    def breath_first_priority_queue(self):
+        path = []
+        path_cost = 0
+
+        queue = PriorityQueue()
+        queue.put((0, self.start))
+        visited = set(self.start)
+
+        branch = {}
+        found = False
+        while not queue.empty():
+            current_node_cost, current_node_delta = queue.get()
+            if current_node_delta == self.goal:
+                print('Found a path.')
+                found = True
+                break
+            else:
+                for action in self.valid_actions(current_node_delta):
+                    next_node_delta = (current_node_delta[0] + action.delta[0], current_node_delta[1] + action.delta[1])
+                    next_node_cost = current_node_cost + action.cost
+                    if next_node_delta not in visited:
+                        queue.put((next_node_cost, next_node_delta))
+                        visited.add(next_node_delta)
+                        branch[next_node_delta] = (next_node_cost, current_node_delta, action)
+
+        if found:
+            n = self.goal
+            path_cost = branch[n][0]
+            while branch[n][1] != self.start:
+                path.append(branch[n][2])
+                n = branch[n][1]
+            path.append(branch[n][2])
+
+        return path[::-1], path_cost
